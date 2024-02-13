2dfd72b66a8d2fbb2b3a1c1b96c981dffcea7cb8
Matija Kukic <kukicmatija@gmail.com>
2024-02-13 19:28:50 +0100

    Visak koda i file-ova za csv format maknut

--- u_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-dataset_path = 'lumen_dataset/data/lumen/export_df.parquet'
-df = pd.read_parquet(dataset_path)
-df.to_csv('obradjeni_pod/data_u_csvu.csv')
-a = list(df['broj_djece_gostiju'])
-print(a)
-print(max(a))
